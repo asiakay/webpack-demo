5afb6273bf50f0d7804b5a582557fd307ba6f36c
Asia K <asialakaygrady@gmail.com>
2022-04-19 01:20:23 -0400

    loading data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import NEWC_Fundraiser_sq from './NEWC_Fundraiser_sq.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component(){
     const element = document.createElement('div');
@@ -14,6 +16,11 @@ const myFlyer = new Image();
 myFlyer.src = NEWC_Fundraiser_sq;
 
 element.appendChild(myFlyer);
+
+console.log(Data);
+console.log(Notes);
+
+
 
     return element;
 }
