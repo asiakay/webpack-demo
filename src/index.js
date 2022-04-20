078f063b68b0dde01fcf6dafc2e8998183051ef6
Asia K <asialakaygrady@gmail.com>
2022-04-19 21:44:42 -0400

    set up HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
 import _ from 'lodash';
-import './style.css';
-import NEWC_Fundraiser_sq from './NEWC_Fundraiser_sq.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from './print.js';
 
 function component(){
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // lodash, now importedby this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
 
       // Add the image to our existing div.
 const myFlyer = new Image();
 myFlyer.src = NEWC_Fundraiser_sq;
 
 element.appendChild(myFlyer);
-
-console.log(Data);
-console.log(Notes);
-
 
 
     return element;
